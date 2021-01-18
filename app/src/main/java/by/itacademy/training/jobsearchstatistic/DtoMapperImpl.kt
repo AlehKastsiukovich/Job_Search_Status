a9faf6d71337aacef1c33aef67f51c8eb1e3d6ad
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2021-01-18 20:57:29 +0300

    add DtoMapperImpl class / util package

---
+++ app/src/main/java/by/itacademy/training/jobsearchstatistic/DtoMapperImpl.kt
@@ -0,0 +1,28 @@
+package by.itacademy.training.jobsearchstatistic
+
+import by.itacademy.training.jobsearchstatistic.domain.Vacancy
+import by.itacademy.training.jobsearchstatistic.model.dto.VacancyDto
+import by.itacademy.training.jobsearchstatistic.util.DtoMapper
+
+class DtoMapperImpl : DtoMapper {
+
+    override fun fromDto(list: List<VacancyDto>): List<Vacancy> {
+        val vacancyList = mutableListOf<Vacancy>()
+        list.forEach { dto ->
+            vacancyList.add(
+                Vacancy(
+                    dto.date.toString(),
+                    dto.company ?: EMPTY_STRING,
+                    dto.source?.name ?: EMPTY_STRING,
+                    dto.sourcePerson ?: EMPTY_STRING,
+                    dto.status?.name ?: EMPTY_STRING
+                )
+            )
+        }
+        return vacancyList
+    }
+
+    companion object {
+        private const val EMPTY_STRING = ""
+    }
+}
