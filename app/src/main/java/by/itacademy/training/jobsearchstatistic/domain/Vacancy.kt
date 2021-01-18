44ed1e3478d87cf35d9734ca89a3f110d19c7e97
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2021-01-18 19:32:25 +0300

    add JobAdapter class/ view/adapter package

--- app/src/main/java/by/itacademy/training/jobsearchstatistic/domain/Vacancy.kt
+++ app/src/main/java/by/itacademy/training/jobsearchstatistic/domain/Vacancy.kt
@@ -1,13 +1,9 @@
 package by.itacademy.training.jobsearchstatistic.domain
 
-import by.itacademy.training.jobsearchstatistic.model.dto.VacancyResource
-import by.itacademy.training.jobsearchstatistic.model.dto.VacancyStatus
-import java.util.Date
-
 data class Vacancy(
-    val date: Date,
+    val date: String,
     val company: String,
-    val source: VacancyResource,
+    val source: String,
     val sourcePerson: String?,
-    val status: VacancyStatus
+    val status: String
 )
--- app/src/main/java/by/itacademy/training/jobsearchstatistic/view/adapter/JobAdapter.kt
+++ app/src/main/java/by/itacademy/training/jobsearchstatistic/view/adapter/JobAdapter.kt
@@ -1,25 +1,46 @@
 package by.itacademy.training.jobsearchstatistic.view.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import by.itacademy.training.jobsearchstatistic.R
+import by.itacademy.training.jobsearchstatistic.databinding.JobItemBinding
 import by.itacademy.training.jobsearchstatistic.domain.Vacancy
 
 class JobAdapter : RecyclerView.Adapter<JobAdapter.JobViewHolder>() {
 
-    private val jobList = mutableListOf<Vacancy>()
+    private val vacancyList = mutableListOf<Vacancy>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.job_item, parent)
+        return JobViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: JobViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(vacancyList[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    fun update(vacancies: List<Vacancy>) {
+        vacancyList.clear()
+        vacancyList.addAll(vacancies)
+        notifyDataSetChanged()
     }
 
-    class JobViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView)
+    override fun getItemCount() = vacancyList.size
+
+    class JobViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val binding: JobItemBinding = JobItemBinding.bind(itemView)
+
+        fun bind(vacancy: Vacancy) {
+            with(binding) {
+                dateTextView.text = vacancy.date
+                companyTextView.text = vacancy.company
+                statusTextView.text = vacancy.status
+                vacancySourceTextView.text = vacancy.source
+                personalityTextView.text = vacancy.sourcePerson
+            }
+        }
+    }
 }
