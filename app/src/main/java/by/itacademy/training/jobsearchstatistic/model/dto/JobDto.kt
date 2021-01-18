5eb5e113286c314522cbf0c6fb50f4dd500c2731
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2021-01-18 19:04:42 +0300

    add Job class / domain package

--- app/src/main/java/by/itacademy/training/jobsearchstatistic/model/dto/JobDto.kt
+++ app/src/main/java/by/itacademy/training/jobsearchstatistic/model/dto/JobDto.kt
@@ -3,9 +3,9 @@ package by.itacademy.training.jobsearchstatistic.model.dto
 import java.util.Date
 
 data class JobDto(
-    val date: Date,
-    val company: String,
-    val resource: JobResource,
-    val resourceName: String,
-    val status: JobStatus
+    val date: Date?,
+    val company: String?,
+    val source: JobResource?,
+    val sourcePerson: String?,
+    val status: JobStatus?
 )
---
+++ app/src/main/java/by/itacademy/training/jobsearchstatistic/view/adapter/JobAdapter.kt
@@ -0,0 +1,22 @@
+package by.itacademy.training.jobsearchstatistic.view.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class JobAdapter : RecyclerView.Adapter<JobAdapter.JobViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: JobViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class JobViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView)
+}
