f18edad8c292dccdb375222c1e0f9adaa9ad75f8
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2021-01-20 16:36:33 +0300

    add CustomViewModelFactory class / viewmodel package

--- app/src/main/java/by/itacademy/training/jobsearchstatistic/view/viewmodel/VacanciesViewModel.kt
+++ app/src/main/java/by/itacademy/training/jobsearchstatistic/view/viewmodel/VacanciesViewModel.kt
@@ -1,12 +1,28 @@
 package by.itacademy.training.jobsearchstatistic.view.viewmodel
 
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataScope
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 import by.itacademy.training.jobsearchstatistic.domain.Vacancy
+import by.itacademy.training.jobsearchstatistic.model.db.VacanciesDatabase
+import by.itacademy.training.jobsearchstatistic.model.repository.VacanciesRepositoryImpl
+import by.itacademy.training.jobsearchstatistic.util.DtoMapperImpl
+import by.itacademy.training.jobsearchstatistic.util.Event
+import kotlinx.coroutines.Dispatchers
 
 class VacanciesViewModel : ViewModel() {
 
-    private var vacanciesLiveData = MutableLiveData<List<Vacancy>>()
+    private var _vacanciesLiveData = MutableLiveData<Event<List<Vacancy>>>()
+    val vacanciesLiveData = _vacanciesLiveData
+    val database = VacanciesDatabase.getDatabase()
+    val mapper = DtoMapperImpl()
+    val repository = VacanciesRepositoryImpl(,mapper)
+
+    fun fetchAllVacancies() {
+        _vacanciesLiveData = liveData(Dispatchers.IO) {
+
+        }
+    }
 
 }
