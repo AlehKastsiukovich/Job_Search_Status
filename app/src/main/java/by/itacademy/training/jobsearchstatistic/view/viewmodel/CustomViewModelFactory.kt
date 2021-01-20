4ee07c2cca7c30ec0c4cefd32218f9c9418c0ccc
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2021-01-20 17:25:34 +0300

    add CustomViewModelFactory class / viewmodel package

--- app/src/main/java/by/itacademy/training/jobsearchstatistic/view/viewmodel/CustomViewModelFactory.kt
+++ app/src/main/java/by/itacademy/training/jobsearchstatistic/view/viewmodel/CustomViewModelFactory.kt
@@ -3,12 +3,20 @@ package by.itacademy.training.jobsearchstatistic.view.viewmodel
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import by.itacademy.training.jobsearchstatistic.model.db.VacanciesDao
+import by.itacademy.training.jobsearchstatistic.model.repository.VacanciesRepositoryImpl
+import by.itacademy.training.jobsearchstatistic.util.DtoMapperImpl
 
-class CustomViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+class CustomViewModelFactory(
+    private val context: Context,
+    private val dao: VacanciesDao,
+    private val mapper: DtoMapperImpl,
+    private val repository: VacanciesRepositoryImpl
+    ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(VacanciesViewModel::class.java)) {
-            return VacanciesViewModel(context) as T
+            return VacanciesViewModel(context, dao, mapper, repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
